06a9c37d3c0f26551d0bdeb8f31f5269ced16b23
Adrian Francisco Lozada Reboceño <adrian.rebocenno@outlook.com>
2025-04-16 09:55:24 -0600

    updated view add permision show

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -61,7 +61,9 @@
                                         <i class="ri-more-fill align-middle"></i>
                                     </button>
                                     <ul class="dropdown-menu dropdown-menu-end">
-                                        <li><a href="{{ route('categories.show', $category->id)}}" class="dropdown-item"><i class="ri-eye-fill align-bottom me-2 text-muted"></i> View</a></li>
+                                        @can('category-show')
+                                            <li><a href="{{ route('categories.show', $category->id)}}" class="dropdown-item"><i class="ri-eye-fill align-bottom me-2 text-muted"></i> View</a></li>
+                                        @endcan
                                         @can('category-edit')
                                         <li><a href="{{ route('categories.edit', $category->id)}}" class="dropdown-item edit-item-btn"><i class="ri-pencil-fill align-bottom me-2 text-muted"></i> Edit</a></li>
                                         @endcan
