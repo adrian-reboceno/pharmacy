782d010aad9b0cdf76ba545f265cb8e8dcd3dcb5
Adrian Francisco Lozada Reboceño <adrian.rebocenno@outlook.com>
2025-04-20 11:27:37 -0600

    datatable only init js basic used

--- resources/views/status/index.blade.php
+++ resources/views/status/index.blade.php
@@ -101,6 +101,6 @@
 <script src="{{ URL::asset('https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js') }}"></script>
 <script src="{{ URL::asset('https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js') }}"></script>
 
-<script src="{{ URL::asset('assets/js/pages/statuses/datatables.init.js') }}"></script>
+<script src="{{ URL::asset('assets/js/pages/datatables/basic.init.js') }}"></script>
 <script src="{{ URL::asset('assets/js/app.js') }}"></script>
 @endsection
