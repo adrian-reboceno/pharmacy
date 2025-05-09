0aea454868a35411743a6788f9d6c421719844f5
Adrian Francisco Lozada Reboceño <adrian.rebocenno@outlook.com>
2025-05-09 09:01:45 -0600

    updated  layouts master add livewire

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,7 +10,7 @@
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico')}}">
     @include('layouts.head-css')
-    
+    @livewireStyles
 </head>
 
 @section('body')
@@ -42,6 +42,8 @@
 
     <!-- JAVASCRIPT -->
     @include('layouts.vendor-scripts')
+    @livewireScripts
+    
 </body>
 
 </html>
